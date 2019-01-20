521fd0544c5051acde128c4643a62a982a4072f5
ampersky <gasttello@gmail.com>
2019-01-20 05:13:37 +0300

    День2.Верстка

--- src/App.js
+++ src/App.js
@@ -1,19 +1,61 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Grid from 'react-bootstrap/lib/Button';
-import Row from 'react-bootstrap/lib/Button';
-import Col from 'react-bootstrap/lib/Button';
+import Grid from 'react-bootstrap/lib/Grid';
+
 import styled from 'styled-components';
+
+import Header from './components/Header'
+import Main from './components/Main'
+import Menu from './components/Menu'
+
+const url = process.env.PUBLIC_URL +'/img/bg.jpg';
+
+const HeaderWrapper=styled.header `
+        width:100%;
+        height: 49px;
+        background-color: #242424;
+        opacity: 0.8;
+
+
+`
+        
+const MenuWrapper = styled.div `
+        height: 90px;
+        padding-top: 24px;
+
+`
  
+const MainWrapper = styled.main `
+        height:600px;
+        padding-top: 130px; 
+        background: url(${url}) no-repeat;
+        background-size: cover;
+`
+
+
 class App extends Component {
   render() {
     return (
-        <div className="wrapper">
-          <SayFulName name="Александр" surname="Акперов" link="https://vk.com/akper_off" />
-          <SayFulName name="Ирина" surname="Акперова" link="https://facebook" />
-          <SayFulName name="Юрий" surname="Дудь" link="a" />
-      </div>  
+        <div className="App">
+          <HeaderWrapper>
+              <Grid>
+                  <Header />
+              </Grid>
+          </HeaderWrapper>
+          <MenuWrapper>
+              <Grid>
+                  <Menu />
+              </Grid>
+          </MenuWrapper> 
+
+          <MainWrapper>
+              <Grid>
+                  <Main />
+              </Grid>
+          </MainWrapper>
+                     
+        </div>
     );
   }
 }
